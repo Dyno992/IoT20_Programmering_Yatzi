c2b896e4151694225b1ad82932b8283f1fefb110
Dino Mangafic <71069904+Dyno992@users.noreply.github.com>
2020-10-07 13:40:38 +0200

    Added: methods for YatziMain & BoardGameMaterial, Yatzi with comments.

--- src/com/company/BoardGameMaterial.java
+++ src/com/company/BoardGameMaterial.java
@@ -1,26 +1,64 @@
 package com.company;
 
 
+import java.util.Scanner;
+
 import static com.company.Die.die;
 
 public class BoardGameMaterial {
     /* Will be used to store all boardgamematerials in one class */
+    private static int turn = 0;
+
 
     public static boolean GameOn() {
-
+        System.out.println("Welcome to Yatzi!");
         return true;
     }
 
-    public static int Turns(int turn) {
+    public static int StartGame() {
 
-        int iturn = 0;
-
-        while (iturn < 3) {
-            System.out.println("Starting turn " + (iturn + 1) + " of 3, rolling dice.");
+        while (turn < 3) {
+            System.out.println("Starting turn " + (turn + 1) + " of 3, rolling dice.");
             for (int i = 0; i < die.length; i++) {
                 die[i].DieRoll();
                 //ds[i].value = 5; //Test if yatzi work
                 System.out.println(i + ": " + die[i].getString());
+            }
+            CheckIfYatzi();
+        }
+        return turn;
+    }
+
+    public static void CheckIfYatzi() {
+
+        boolean yatzi = true;
+        for (int j = 1; j < 5; j++) {
+            if (die[j].value != die[j - 1].value) {
+                //Set flag to false
+                yatzi = false;
+            }
+        }
+        if (yatzi) {
+            System.out.println("You got YATZI! in " + die[0].value + "'s");
+
+        } else {
+            //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
+            if (turn != 2) {
+                System.out.println("Want to throw again? (y for yes, anything else for no)");
+                Scanner sc = new Scanner(System.in);
+                if (sc.next().equals("y")) {
+                    ++turn;
+                } else {
+                    System.exit(0);
+                }
+            } else {
+                System.out.println("Game over! Want to play again?");
+                Scanner sc = new Scanner(System.in);
+                if (sc.next().equals("y")) {
+                    turn = 0;
+                } else {
+                    System.exit(0);
+                }
             }
         }
     }
