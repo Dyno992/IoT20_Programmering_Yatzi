924e2a5d1fabf7bf2834fdc074bc6df3129aa1ac
Dino Mangafic <71069904+Dyno992@users.noreply.github.com>
2020-10-07 14:49:04 +0200

    Added new methods.

--- src/com/company/BoardGameMaterial.java
+++ src/com/company/BoardGameMaterial.java
@@ -1,65 +1,76 @@
 package com.company;
 
-
 import java.util.Scanner;
+import static com.company.Die.*;
+class BoardGameMaterial {
 
-import static com.company.Die.die;
-
-public class BoardGameMaterial {
-    /* Will be used to store all boardgamematerials in one class */
     private static int turn = 0;
-
-
-    public static boolean GameOn() {
+    private static boolean GameOn() {
         System.out.println("Welcome to Yatzi!");
         return true;
     }
 
-    public static int StartGame() {
-
-        while (turn < 3) {
-            System.out.println("Starting turn " + (turn + 1) + " of 3, rolling dice.");
-            for (int i = 0; i < die.length; i++) {
-                die[i].DieRoll();
-                //ds[i].value = 5; //Test if yatzi work
-                System.out.println(i + ": " + die[i].getString());
+    static void StartGame() {
+        CreateDice();
+        while (GameOn()) {
+            while (turn < 3) {
+                System.out.println("Starting turn " + (turn + 1) + " of 3, rolling dice.");
+                for (int i = 0; i < die.length; i++) {
+                    die[i].DieRoll();
+//                    die[i].value = 5; //Test if yatzi work
+                    System.out.println(i + ": " + die[i].getString());
+                }
+                CheckIfYatzi();
             }
-            CheckIfYatzi();
         }
-        return turn;
     }
 
-    public static void CheckIfYatzi() {
+    private static void CheckIfYatzi() {
 
         boolean yatzi = true;
         for (int j = 1; j < 5; j++) {
             if (die[j].value != die[j - 1].value) {
                 //Set flag to false
                 yatzi = false;
+                break;
             }
         }
         if (yatzi) {
-            System.out.println("You got YATZI! in " + die[0].value + "'s");
 
+            System.out.println("You got YATZI! in " + die[0].value + "'s");
+            System.out.println("Program Ending...");
+            System.exit(0);
         } else {
-            //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
-            if (turn != 2) {
-                System.out.println("Want to throw again? (y for yes, anything else for no)");
-                Scanner sc = new Scanner(System.in);
-                if (sc.next().equals("y")) {
-                    ++turn;
-                } else {
-                    System.exit(0);
-                }
+            NotYatzi();
+        }
+    }
+
+    private static void NotYatzi() {
+
+        //Here we check if there is no Yatzy: then we check what turn we are on and asks the player if we want to continue or not
+        if (turn != 2) {
+            System.out.println("Want to throw again? (y for yes, anything else for no)");
+            Scanner sc = new Scanner(System.in);
+            if (sc.next().equals("y")) {
+                ++turn;
             } else {
-                System.out.println("Game over! Want to play again?");
-                Scanner sc = new Scanner(System.in);
-                if (sc.next().equals("y")) {
-                    turn = 0;
-                } else {
-                    System.exit(0);
-                }
+                System.out.println("Program Ending...");
+                System.exit(0);
+
             }
+        } else {
+            GameOver();
+        }
+    }
+
+    private static void GameOver() {
+        System.out.println("Game over! Want to play again?");
+        Scanner sc = new Scanner(System.in);
+        if (sc.next().equals("y")) {
+            turn = 0;
+        } else {
+            System.out.println("Program Ending...");
+            System.exit(0);
         }
     }
 }
--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -1,37 +1,25 @@
 package com.company;
 
-public class Die extends com.company.BoardGameMaterial {
-    public int value = 0;
+class Die extends com.company.BoardGameMaterial {
 
-    public static Die[] die;
-
-    public Die() {
+    int value;
+    static Die[] die;
+    private Die() {
         value = (int) Math.random();
     }
 
-    public int DieRoll() {
+    void DieRoll() {
         value = (int)(Math.random()*6+1);
-        return value;
     }
 
-    public int DieReroll() {
-        return DieRoll();
-    }
-
-    public String getString() {
+    String getString() {
         return "Dice shows " + value;
     }
 
-   public static void CreateDice() {
-
-        die = new Die[5];
-
-        for (int i = 0; i < 5; i++) {
-            die[i] = new Die();
-        }
-
-
-       return;
+    static void CreateDice() {
+      die = new Die[5];
+      for (int i = 0; i < 5; i++) {
+         die[i] = new Die();
+      }
    }
-
 }
