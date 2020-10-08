fcf69607558fb9bc76005c6d16f6f09ad119b974
DESKTOP-MG1KFQA\Besim <besim.musliu@student.kyh.se>
2020-10-08 08:45:55 +0200

    Changed all methods to lowerCase. :)

--- src/com/company/Die.java
+++ src/com/company/Die.java
@@ -8,11 +8,11 @@ class Die extends com.company.BoardGameMaterial {
         value = (int) Math.random();
     }
 
-    void DieRoll() {
+    void dieRoll() {
         value = (int) (Math.random() * 6 + 1);
     }
 
-    static void CreateDice() {
+    static void createDice() {
         dice = new Die[5];
         for (int i = 0; i < 5; i++) {
             dice[i] = new Die();
--- src/com/company/YahtzeeMain.java
+++ src/com/company/YahtzeeMain.java
@@ -6,6 +6,6 @@ public class YahtzeeMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Yahtzee!");
-        StartGame();
+        startGame();
     }
 }
